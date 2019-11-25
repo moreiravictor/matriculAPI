b3a0e1ffae05497256f3cc58f9834bc75f62a63c
Victor Moreira <victormoreira176@hotmail.com>
2019-11-24 21:23:01 -0300

    getSubjectModel method/getBySubjectName method/semi-test/Using the Subject pojo

--- src/main/java/matriculAPI/application/matriculAPIapp.java
+++ src/main/java/matriculAPI/application/matriculAPIapp.java
@@ -3,18 +3,19 @@ package matriculAPI.application;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import matriculAPI.model.Subject;
 import matriculAPIController.*; 
 public class matriculAPIapp {
 	public static void main (String args[]) throws IOException {
 		
 		DataController controll = new DataController();
-		ArrayList<String> subjectList = controll.listSubjectsInfo("matricula.xlsx");
-		for (String s : subjectList) {
+		ArrayList<Subject> subjectList = controll.listSubjectsInfo("matricula.xlsx");
+		for (Subject s : subjectList) {
 			System.out.println(s);
 		}
 		System.out.println("=================================================================");
-		ArrayList<String> specificList = controll.getBySubjectName("Projeto Dirigido", "matricula.xlsx");
-		for (String s : specificList) {
+		ArrayList<Subject> specificList = controll.getBySubjectName("Projeto Dirigido", "matricula.xlsx");
+		for (Subject s : specificList) {
 			System.out.println(s);
 		}
 	}
